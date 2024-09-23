cb40b014ad3e471fccfd32aa10e9ee77cd1f26bf
Tao_ta <745103130@qq.com>
2024-09-23 12:15:44 +0800

    feat: new entry.

--- server/static/webroot/src/views/entry.vue
+++ server/static/webroot/src/views/entry.vue
@@ -1,126 +1,293 @@
 <script setup lang="ts">
 //base
-import { onMounted, onUnmounted, ref } from 'vue';
-import { NIcon, NPopover } from 'naive-ui'
+import { onMounted, onUnmounted, ref } from "vue";
+import { NIcon, NPopover } from "naive-ui";
 //api
-import { GetEntryInfo } from '@/apis/api_common';
+import { GetEntryInfo } from "@/apis/api_common";
 //icon
-import { LogoGithub, LogoTwitter, LogoWechat, Mail } from '@vicons/ionicons5'
-import { QuestionCircleOutlined } from '@vicons/antd';
+import { LogoGithub, LogoTwitter, LogoWechat, Mail } from "@vicons/ionicons5";
+import { QuestionCircleOutlined } from "@vicons/antd";
 //ref
-const dateDiff = ref<string>("")
-const yesterDayPV = ref<number>(0)
-const monthPV = ref<number>(0)
-const articleCount = ref<number>(0)
-const pageHeight = ref<number>(window.innerHeight)
+const dateDiff = ref<string>("");
+const yesterDayPV = ref<number>(0);
+const monthPV = ref<number>(0);
+const articleCount = ref<number>(0);
+const pageHeight = ref<number>(window.innerHeight);
 //hook
 let timer: number | undefined;
 onMounted(async () => {
-    let res = await GetEntryInfo();
-    if (res.ok) {
-        let day = new Date(res.data.JSTimeStamp)
-        let now = new Date()
-        let diff = Math.abs(now.getTime() - day.getTime())
-        const oneDay = 86400000; // 24 * 60 * 60 * 1000
-        dateDiff.value = `${Math.floor(diff / oneDay)}天`;
-        yesterDayPV.value = res.data.YesterdayCount
-        monthPV.value = res.data.CurrentMonthCount
-        articleCount.value = res.data.ArticleCount
-    }
-    timer = setInterval(autoChangeName, 300)
-    document.addEventListener('scroll', atPage)
-})
+  await getInfo();
+  timer = setInterval(autoChangeName, 300);
+  document.addEventListener("scroll", atPage);
+});
 
-const imTag = ["Golang工程师", "云原生小白", "前端爱好者", "学生", "币圈玩家", "摄影佬", "EDM制作人"]
+const getInfo = async () => {
+  let res = await GetEntryInfo();
+  if (res.ok) {
+    let day = new Date(res.data.JSTimeStamp);
+    let now = new Date();
+    let diff = Math.abs(now.getTime() - day.getTime());
+    const oneDay = 86400000; // 24 * 60 * 60 * 1000
+    dateDiff.value = `${Math.floor(diff / oneDay)}天`;
+    yesterDayPV.value = res.data.YesterdayCount;
+    monthPV.value = res.data.CurrentMonthCount;
+    articleCount.value = res.data.ArticleCount;
+  }
+};
+
+const imTag = [
+  "Golang工程师",
+  "云原生小白",
+  "前端爱好者",
+  "学生",
+  "币圈玩家",
+  "摄影佬",
+  "EDM制作人",
+];
 
 // 动态变化
-const titleName = ref("")
+const titleName = ref("");
 const painterList = [
-    "Painter",
-    "Pai|nter",
-    "Pai nter",
-    "Pai|nter",
-    "Pa|nter",
-    "P|nter",
-    "|nter",
-    " nter",
-    "E|nter",
-    "E|nter",
-    "Enter",
-]
-let currentIndex = 0
+  "Painter",
+  "Pai|nter",
+  "Pai nter",
+  "Pai|nter",
+  "Pa|nter",
+  "P|nter",
+  "|nter",
+  " nter",
+  "E|nter",
+  "E|nter",
+  "Enter",
+];
+let currentIndex = 0;
 const autoChangeName = () => {
-    if (currentIndex >= painterList.length - 1) {
-        clearInterval(timer)
-    }
-    titleName.value = painterList[currentIndex]
-    currentIndex++
-}
+  if (currentIndex >= painterList.length - 1) {
+    clearInterval(timer);
+  }
+  titleName.value = painterList[currentIndex];
+  currentIndex++;
+};
+
+const pageTranstion = ref<{
+  [key: number]: { opacity: number; offset: number };
+}>({
+  1: { opacity: 0, offset: 0 },
+  2: { opacity: 0, offset: 0 },
+});
+const pageAt = ref<number>(0);
 
 const scrollTo = (pos: number) => {
-    window.scrollTo({ left: 0, top: pos, behavior: "smooth" })
-}
+  window.scrollTo({ left: 0, top: pos, behavior: "smooth" });
+};
 
 const scrollToPage = (page: number) => {
-    let pos = (pageHeight.value - 60) * (page - 1)
-    scrollTo(pos)
-}
+  let pos = (pageHeight.value - 60) * (page - 1);
+  scrollTo(pos);
+};
 
-const atPage = () => {
+const atPage = (e: any) => {
+  pageAt.value = e.target.scrollingElement.scrollTop / (pageHeight.value - 60);
+  fixPage(1,e);
+  fixPage(2,e);
+};
 
-}
+const pagePosition = (page: number) => {
+  return (pageHeight.value - 60) * page;
+};
+
+const fixPage = (page: number,e: any) => {
+  const range = 0.2;
+  let atPage: number = pageAt.value;
+  let distantTo = distantToFix(atPage, page, range);
+  if (distantTo > 0.7) {
+    distantTo = 1;
+  }
+  pageTranstion.value[page].opacity = distantTo;
+  pageTranstion.value[page].offset = e.target.scrollingElement.scrollTop - pagePosition(page) ;
+};
+
+const rangeAt = (at: number, fix: number, range: number): boolean => {
+  if (at < fix + range && at > fix - range) {
+    return true;
+  }
+  return false;
+};
+
+const distantToFix = (at: number, fix: number, range: number): number => {
+  if (!rangeAt(at, fix, range)) {
+    return 0;
+  }
+  if (at > fix) {
+    return 1 - (at - fix) / range;
+  } else {
+    return 1 - (fix - at) / range;
+  }
+};
 
 const goLink = (link: string) => {
-    window.open(link, "_blank")
-}
+  window.open(link, "_blank");
+};
 </script>
 <template>
-    <div class="entry-cont">
-        <div class="entry-page">
-            <span class="entry-painter-text"
-                @click="scrollToPage(2)">
-                {{ titleName }}
-            </span>
-        </div>
-        <div class="entry-page">
-            Page2
-        </div>
-        <div class="entry-page">
-            Page3
-        </div>
+  <div class="entry-cont">
+    <div class="entry-page">
+      <span class="entry-painter-text" @click="scrollToPage(2)">
+        {{ titleName }}
+      </span>
     </div>
+    <div
+      class="entry-page entry-page-bold"
+      :style="{
+        opacity: pageTranstion[1].opacity,
+        transform: 'translateY(' + pageTranstion[1].offset + 'px)',
+      }"
+    >
+      <span class="second-highlight"> 你好, </span>
+      &nbsp;欢迎来到
+      <span> Painter 👋 </span>
+      <span style="vertical-align: super; margin: 0 5px">
+        <n-popover>
+          <template #trigger>
+            <n-icon size="18">
+              <QuestionCircleOutlined />
+            </n-icon>
+          </template>
+          <div>
+            <div>Painter是一个通过Golang与Vue进行搭建的 社区博客 框架</div>
+            <div>目的是为了建立一个分享技术笔记以及日常的地方</div>
+          </div>
+        </n-popover>
+      </span>
+    </div>
+    <div
+      class="entry-page entry-page-bold"
+      :style="{
+        opacity: pageTranstion[2].opacity,
+        transform: 'translateY(' + pageTranstion[2].offset + 'px)',
+      }"
+    >
+      <div>
+        <div style="display: flex; align-items: center">
+          我是
+          <span class="second-highlight"> 逃塔 </span>
+        </div>
+
+        <span v-for="tag in imTag" class="entry-tag">
+          {{ tag }}
+        </span>
+        <div style="height: 10px"></div>
+        <div class="entry-contact-me">
+          <div
+            class="entry-icon-cont"
+            @click="goLink('https://github.com/callme-taota')"
+          >
+            <n-icon size="20">
+              <logo-github />
+            </n-icon>
+          </div>
+          <div
+            class="entry-icon-cont"
+            @click="goLink('https://twitter.com/Taota_chen')"
+          >
+            <n-icon size="20">
+              <logo-twitter />
+            </n-icon>
+          </div>
+          <div
+            class="entry-icon-cont"
+            @click="goLink('http://www.callmetaota.fun/wechat.jpeg')"
+          >
+            <n-icon size="20">
+              <logo-wechat />
+            </n-icon>
+          </div>
+          <div
+            class="entry-icon-cont"
+            @click="goLink('mailto:taota.chen@gmail.com')"
+          >
+            <n-icon size="20">
+              <mail />
+            </n-icon>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style>
 .entry-cont {
-    min-height: calc(100vh - 80px);
-    display: flex;
-    flex-direction: column;
-    padding: 0 80px 20px 80px;
-    position: relative;
+  min-height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  padding: 0 80px 20px 80px;
+  position: relative;
 }
 
-.entry-page{
-    position: relative;
-    height: calc(100vh - 60px);
-    display: flex;
-    align-items: center;
-    justify-content: center;
+.entry-page {
+  position: relative;
+  height: calc(100vh - 60px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .entry-painter-text {
-    font-weight: 500;
-    font-size: xx-large;
-    padding: 0 18px;
-    cursor: pointer;
-    transition: 0.08s;
-    text-align: center;
-    transition: 0.3s;
+  font-weight: 500;
+  font-size: xx-large;
+  padding: 0 18px;
+  cursor: pointer;
+  transition: 0.08s;
+  text-align: center;
+  transition: 0.3s;
 }
 
 .entry-painter-text:hover {
-    text-shadow: 0 0 12px #fff;
-    color: var(--color-rev);
-    background-color: var(--base-hover-background);
+  text-shadow: 0 0 12px #fff;
+  color: var(--color-rev);
+  background-color: var(--base-hover-background);
 }
 
+.second-highlight {
+  color: var(--second-highlight-color);
+}
+
+.entry-page-bold {
+  font-weight: bold;
+  font-size: xx-large;
+  transition: opacity 0.05s ease-in-out;
+}
+
+.entry-tag {
+  margin: 4px 8px 4px 0;
+  padding: 2px 4px 2px 4px;
+  font-size: large;
+  font-weight: lighter;
+  font-style: italic;
+  border-bottom: 1px solid var(--highlight-color);
+}
+
+.entry-contact-me {
+  flex-wrap: wrap;
+  display: flex;
+  margin-top: 8px;
+}
+
+.entry-icon-cont {
+  transition: 0.3s;
+  border-radius: 14px;
+  height: 40px;
+  width: 40px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  justify-content: center;
+  background: var(--layout--bottom-background);
+  margin-right: 8px;
+}
+
+.entry-icon-cont:hover {
+  background-color: var(--base-hover-background);
+  color: var(--btn-hover-color);
+}
 </style>
